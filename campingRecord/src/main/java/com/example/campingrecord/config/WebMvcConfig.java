854c313388cb64a2aa1926dd9012466d7550ef3a
chuanyi.shi <chuanyi.shi@pactera.com>
2022-07-27 14:05:56 +0800

    fix bug cors config

--- campingRecord/src/main/java/com/example/campingrecord/config/WebMvcConfig.java
+++ campingRecord/src/main/java/com/example/campingrecord/config/WebMvcConfig.java
@@ -3,12 +3,15 @@ package com.example.campingrecord.config;
 import com.example.campingrecord.interceptor.JwtInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
+import java.util.Collections;
 
 /**
  * 跨域
@@ -19,14 +22,18 @@ import javax.servlet.Filter;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*")
-                .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS").allowedHeaders("*")
-                .exposedHeaders("access-control-allow-headers", "access-control-allow-methods",
-                        "access-control-allow-origin", "access-control-max-age", "X-Frame-Options")
-                .allowCredentials(true).maxAge(3600);
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedOriginPatterns(Collections.singletonList("*"));
+//        corsConfiguration.addAllowedOrigin("*"); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+        corsConfiguration.setMaxAge(3600L);
+        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
     }
 
     @Bean
